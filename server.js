115218299402ddea6ec363fa96e930283bb05038
Aneeskhattak085 <36186083+Aneeskhattak085@users.noreply.github.com>
2018-11-01 19:37:45 +0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const Chatkit = require('pusher-chatkit-server')
+
+const app = express()
+
+const chatkit = new Chatkit.default({
+  instanceLocator: 'v1:us1:1d933e5a-5d16-45a4-8ada-8a3b36ad0560',
+  key: '50ec5b72-f5d6-442f-925a-ddb627784a61:7ZAn6st2rAu1UtZHcp5sZAw4AvTFsk0VKAlcsz/027I=',
+})
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
+app.post('/users', (req, res) => {
+  const { username } = req.body
+  chatkit
+    .createUser({ 
+	id: username, 
+	name: username 
+     })
+  .then(() => res.sendStatus(201))
+    .catch(error => {
+      if (error.error_type === 'services/chatkit/user_already_exists') {
+        res.sendStatus(200)
+      } else {
+        res.status(error.status).json(error)
+      }
+    })
+})
+
+app.post('/authenticate', (req, res) => {
+  const authData = chatkit.authenticate({ userId: req.query.user_id })
+  res.status(authData.status).send(authData.body)
+})
+
+
+const PORT = 3001
+app.listen(PORT, err => {
+  if (err) {
+    console.error(err)
+  } else {
+    console.log(`Running on port ${PORT}`)
+  }
+})
